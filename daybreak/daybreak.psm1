facd8e6a0e0c5b31580a1af03816febc62f2ec23
Karl Prosser <klumsy@gmail.com>
2014-04-17 23:53:54 -0700

    add some constant number variables 0 - 10

--- daybreak/daybreak.psm1
+++ daybreak/daybreak.psm1
@@ -40,5 +40,29 @@ set-alias 言 write-host
 #endregion
 
 #region number and other helpers , comparatives
+#this will have lots of comparisions build in eventually.
 
+function 比较 ($第一件事,$比,[switch]$大)
+{
+ if ($大)
+ {
+   return ($第一件事 -gt $比)
+ }
+ else { throw "other comparisions not yet supported" }
+}
 #endregion
+
+#region numbers
+Set-Variable -Name 零 -Value 0 -Scope global -Option ReadOnly
+Set-Variable -Name 一 -Value 1 -Scope global -Option ReadOnly
+Set-Variable -Name 二 -Value 2 -Scope global -Option ReadOnly
+Set-Variable -Name 三 -Value 3 -Scope global -Option ReadOnly
+Set-Variable -Name 四 -Value 4 -Scope global -Option ReadOnly
+Set-Variable -Name 五 -Value 5 -Scope global -Option ReadOnly
+Set-Variable -Name 六 -Value 6 -Scope global -Option ReadOnly
+Set-Variable -Name 七 -Value 7 -Scope global -Option ReadOnly
+Set-Variable -Name 八 -Value 8 -Scope global -Option ReadOnly
+Set-Variable -Name 九 -Value 9 -Scope global -Option ReadOnly
+Set-Variable -Name 十 -Value 10 -Scope global -Option ReadOnly
+
+#end
