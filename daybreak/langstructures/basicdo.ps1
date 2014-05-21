557edc069bce53b52bb1294417a1d05ba1646987
Karl Prosser <klumsy@gmail.com>
2014-05-21 00:03:34 -0700

    made do not use invoke-expression but script blocks, and added do example

--- daybreak/langstructures/basicdo.ps1
+++ daybreak/langstructures/basicdo.ps1
@@ -1,12 +1,17 @@
 ﻿#simple do-while loop
-function 先重复 ($做,$当=$false)
+function 先重复 ($做,$当={$false})
 {
  if (!($当 -is [scriptblock] ))
  {
    #"Only scriptblocks for the condition 当"
    throw "条件'当'只接受脚本块."
  }
- Invoke-Expression ( "do{
-  $做
- }while($当)")
+ if (!($做 -is [scriptblock] ))
+ {
+   #"Only scriptblocks for the condition 当"
+   throw "条件'做'只接受脚本块."
+ }
+ do{
+  . $做
+ }while(. $当)
 }
