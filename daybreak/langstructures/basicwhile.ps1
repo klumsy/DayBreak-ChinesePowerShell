b154610fe0ca86180133160f391e4dedd8fabf12
Karl Prosser <klumsy@gmail.com>
2014-05-20 23:49:35 -0700

    update while function to not use invoke-expression but execute scriptblocks

--- daybreak/langstructures/basicwhile.ps1
+++ daybreak/langstructures/basicwhile.ps1
@@ -6,8 +6,13 @@ function 重复 ($做,$当)
    #"Only scriptblocks for the condition 当"
    throw "条件'当'只接受脚本块."
  }
- Invoke-Expression ( "while($当)
+  if (!($做 -is [scriptblock] ))
  {
-  $做
- }")
+   #"Only scriptblocks for the condition 当"
+   throw "条件'做'只接受脚本块."
+ }
+while(. $当) #preform comparision and execution in the same scope. 
+ {
+  . $做
+ }
 }
