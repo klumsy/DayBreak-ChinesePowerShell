51147e9ef8b3118643c4d1b8b8cc0980c520f83d
Mooser Lee <mosser@live.cn>
2014-05-31 15:13:22 +0800

    1.Add like, notlike, match,notmatch,in, notin operators into funcation '比较' 2.Add 2 examples file for logic and comparision operators

--- daybreak/operators/comparisons.ps1
+++ daybreak/operators/comparisons.ps1
@@ -2,7 +2,7 @@
 {
  param(
  $前者,
- [ValidateSet('等于','不等于','大于', '大于等于','小于', '小于等于')]
+ [ValidateSet('等于','不等于','大于', '大于等于','小于', '小于等于','相似','不相似','匹配','不匹配','包含','不包含','在','不在')]
  $条件,
  $后者
  )
@@ -14,6 +14,14 @@
    '大于等于'{ return $前者 -ge $后者}  # large than or equal
    '小于'{ return $前者 -lt $后者 }     # less than
    '小于等于'{ return $前者 -le $后者 } # less than ro equal
+   '相似'{ return $前者 -like $后者 }   # like
+   '不相似'{ return $前者 -notlike $后者 }   # not like
+   '匹配'{ return $前者 -match $后者 }       # match
+   '不匹配'{ return $前者 -notmatch $后者 }  # not match
+   '包含'{ return $前者 -contains $后者 }    # contains
+   '不包含'{ return $前者 -notcontains $后者 } # not contains
+   '在'{ return $前者 -in $后者 }            # in
+   '不在'{ return $前者 -notin $后者 }       # not in
    default { return '其它的条件运算暂不支持.'} # other comparisions not yet supported
  }
 }
---
+++ examples/ComparisonsOperatersExamples.ps1
@@ -0,0 +1,23 @@
+﻿# contains
+比较 3,4,6 包含 4
+
+# not contains
+比较 3,4,6 不包含 5
+
+# like
+比较 'mistool' 相似 '*tool'
+
+# not like
+比较 'mistool' 不相似 '*too'
+
+# match
+比较 'mistool' 匹配 'to{2}l'
+
+# not match
+比较 'mistool' 不匹配 'to{3}l'
+
+# in
+比较 4 在 4,6
+
+# not in
+比较 5 不在 4,6
